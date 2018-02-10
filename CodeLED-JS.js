607ffaeb1b074b645c65c29f69a8c1b469639c52
Ultime 5528 <keranna.first@gmail.com>
2018-02-10 15:54:51 -0500

    FinalLED

--- CodeLED-JS.js
+++ CodeLED-JS.js
@@ -7,19 +7,23 @@ var strip = new Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
 
 var rouge = strip.Color(255, 0, 0);
 var bleu = strip.Color(0, 0, 255);
+var noir = strip.Color(0, 0, 0);
+var vert = strip.Color(0, 255, 0);
+var jaune = strip.Color(255, 255, 0);
+var blanc = strip.Color(255, 255, 255);
+
 
 function setup() {
   strip.setBrightness(BRIGHTNESS);
   strip.begin();
   strip.setPixelColor(0, 0x00FF00);
-    strip.show();
-    
+  strip.show();
 }
 
 function loop() {
-    debutMatch(temps);
-    temps++;
-
+  //signal(temps);
+  monter(temps);
+  temps++;
 }
 
 
@@ -30,7 +34,6 @@ function debutMatch(temps) {
 	for (var i = 0; i < NUM_LEDS; i++) {
 		
 		y = 0.5 * Math.sin (2 * Math.PI / longueurCycle * (i + temps)) + 0.5;
-	//	y = ((i + temps) % 60) / 60; 
 		
 		strip.setPixelColor (i, interpolate(rouge, bleu, y)); //interpolate(rouge, bleu, y));
 		
@@ -39,12 +42,136 @@ function debutMatch(temps) {
 	delay(50);
 }
 
-function interpolate(a, b, t) {
+
+function endGame(temps) {
+	var longueurCycle = 60;
+	var y;
 	
-	return (((a & 0xff0000) + ((b & 0xff0000) - (a & 0xff0000)) * t) & 0xff0000) | (((a & 0x00ff00) + ((b & 0x00ff00) - (a & 0x00ff00)) * t) & 0x00ff00) | (((a & 0x0000FF) + ((b & 0x0000FF) - (a & 0x0000FF)) * t)  & 0x0000ff)
+	for (var i = 0; i < NUM_LEDS; i++) {
+		
+		y = 1 - ((i + temps) % longueurCycle) / longueurCycle
+		
+		strip.setPixelColor (i, interpolate(noir, rouge, y)); //interpolate(rouge, bleu, y));
+		
+	}
+	strip.show();
+	delay(50);
+}
+
+function signal1(temps) {
+	var longueurCycle = 60;
+	
+	var y = -2/longueurCycle * Math.abs((temps % longueurCycle) - longueurCycle/2) + 1;
+	
+	for (var i = 0; i < NUM_LEDS; i++) {
+		
+		strip.setPixelColor(i, interpolate(noir, vert, y)); //interpolate(rouge, bleu, y));
+		
+	}
+	strip.show();
+	delay(10);
+}
+
+function signal2(temps) {
+	var longueurCycle = 60;
+	
+	var y = -2/longueurCycle * Math.abs((temps % longueurCycle) - longueurCycle/2) + 1;
+	
+	for (var i = 0; i < NUM_LEDS; i++) {
+		
+		strip.setPixelColor(i, interpolate(noir, jaune, y)); //interpolate(rouge, bleu, y));
+		
+	}
+	strip.show();
+	delay(10);
+}
+
+function autonome(temps) {
+	var longueurCycle = 20;
+	var y;
+	var distance = 1;
+	
+	for (var i = 0; i < NUM_LEDS; i++) {
+		y = distance * (Math.sin(2 * Math.PI / longueurCycle * (i + temps)) - 1) + 1;
+		
+		strip.setPixelColor(i, interpolate(bleu, blanc, y));
+	}
+	strip.show();
+	delay(50);
+}
+
+function monter(temps) {
+	var longueurCycle = 15;
+	var y;
+	var distance = 1;
+	
+	temps = temps % (2 * longueurCycle);
+	
+	for (var i = 0; i < NUM_LEDS; i++) {
+		y = (i % longueurCycle) - longueurCycle + Math.abs(temps % longueurCycle - longueurCycle);
+		
+		
+		if(temps < longueurCycle) {
+			if(y < 0)
+				strip.setPixelColor (i, blanc);
+			else
+				strip.setPixelColor (i, bleu);
+		}
+		else {
+			if(y < 0)
+				strip.setPixelColor (i, bleu);
+			else
+				strip.setPixelColor (i, blanc);
+		}
+		
+	}
+	strip.show();
+	delay(50);
 }
 
 
+function teleop(temps) {
+	var longueurCycle = 60;
+	var y;
+	
+	temps %= longueurCycle;
+	
+	for (var i = 0; i < NUM_LEDS; i++) {
+		
+		y = Math.sin(2 * Math.PI / (longueurCycle / 5) * (i + temps)) + 2 * (-1 - 2.25) / longueurCycle * Math.abs(temps - (longueurCycle / 2.0)) + 2.25;
+		
+		if(y > 0)
+			strip.setPixelColor (i, interpolate(bleu, blanc, y)); //interpolate(rouge, bleu, y));
+		else
+			strip.setPixelColor (i, bleu);
+		
+	}
+	strip.show();
+	delay(50);
+}
 
+function cube(temps) {
+	var longueurCycle = 60;
+	var y;
+	
+	temps %= longueurCycle;
+	
+	for (var i = 0; i < NUM_LEDS; i++) {
+		
+		y = Math.sin(2 * Math.PI / (longueurCycle / 5) * (i + temps)) + 2 * (-1 - 2.25) / longueurCycle * Math.abs(temps - (longueurCycle / 2.0)) + 2.25;
+		
+		if(y > 0)
+			strip.setPixelColor (i, interpolate(bleu, jaune, y)); //interpolate(rouge, bleu, y));
+		else
+			strip.setPixelColor (i, bleu);
+		
+	}
+	strip.show();
+	delay(50);
+}
 
-
+function interpolate(a, b, t) {
+	t = t > 1 ? 1 : t < 0 ? 0 : t;
+	
+	return (((a & 0xff0000) + ((b & 0xff0000) - (a & 0xff0000)) * t) & 0xff0000) | (((a & 0xff00) + ((b & 0xff00) - (a & 0xff00)) * t) & 0xff00) | (((a & 0xff) + ((b & 0xff) - (a & 0xff)) * t)  & 0xff)
+}
